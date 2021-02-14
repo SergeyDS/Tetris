ef25ca696487bffd4f0e7e706239f6f7e457a005
Sergey <sergey_1c@mail.ru>
2021-02-14 13:38:09 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,28 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Программисты С#");
+            Console.SetWindowSize(40, 40);
+            Console.SetBufferSize(40, 40);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Drow(x1, y1, c1);
+
+            int x2 = 3;
+            int y2 = 4;
+            char c2 = '#';
+            Drow(x2, y2, c2);
+
+
+
+            Console.ReadLine();
+        }
+        static void Drow(int x,int y,char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(c);
         }
     }
 }
