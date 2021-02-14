02e9211ee38c94be2e66447fd01b2cbbf1bb0a94
Sergey <sergey_1c@mail.ru>
2021-02-14 21:31:08 +0700

    Добавил класс Square
    Добавил класс Square

--- Program.cs
+++ Program.cs
@@ -10,7 +10,32 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Программисты С#");
+            Console.SetWindowSize(40, 40);
+            Console.SetBufferSize(40, 40);
+
+            Square s = new Square(10,15,'*');
+            s.Draw();
+
+            Point p1 = new Point();
+
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+
+            p1.Draw();
+
+            Point p2 = new Point()
+            {
+                x = 4,
+                y = 5,
+                c = '#',
+            };
+
+
+            p2.Draw();
+
+            Console.ReadLine();
         }
+
     }
 }
