b3197d6a50fd4a120beddbbe7cfd88a669328b08
Sergey <sergey_1c@mail.ru>
2021-02-15 22:01:18 +0700

    Move
    Организовал движение фигур вверх,вниз,и вправо и влево

---
+++ Point.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+  public class Point
+    {
+        public int x;
+        public int y;
+        public char c;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(c);
+        }
+
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+
+        internal void Move(Direction dir)
+        {
+            switch(dir)
+            {
+                case Direction.DOWN:
+                    y += 1;
+                    break;
+                case Direction.LEFT:
+                    x -= 1;
+                    break;
+                case Direction.RIGHT:
+                    x += 1;
+                    break;
+
+
+            }
+        }
+
+        internal void Hide()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+        }
+
+        public Point() { }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tetris
@@ -10,7 +11,28 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Программисты С#");
+            Console.SetWindowSize(40, 40);
+            Console.SetBufferSize(40, 40);
+
+            
+
+            Square s = new Square(2,5,'*');
+            s.Draw();
+            
+            Thread.Sleep(500);
+            s.Hide();
+            s.Move(Direction.LEFT);
+            s.Draw();
+
+            
+
+            //Stick stick = new Stick(6, 6, '*');
+            //stick.Draw();
+
+
+
+            Console.ReadLine();
         }
+
     }
 }
